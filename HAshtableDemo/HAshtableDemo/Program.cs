234eeb3cb11f4d11355fd329cc7efe0bd9273971
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-18 01:25:45 +0530

    [Pratibha Mastud] Add: Find Frequency word form Paragraph

--- HAshtableDemo/HAshtableDemo/Program.cs
+++ HAshtableDemo/HAshtableDemo/Program.cs
@@ -7,7 +7,7 @@ namespace HAshtableDemo
         static void Main(string[] args)
         {
             MyMapNod<int, string> myMapNod = new MyMapNod<int, string>(15);
-            myMapNod.Add(0,"To");
+            /*myMapNod.Add(0, "To");
             myMapNod.Add(1, "be");
             myMapNod.Add(2, "or");
             myMapNod.Add(3, "not");
@@ -15,9 +15,23 @@ namespace HAshtableDemo
             myMapNod.Add(5, "be");
             int index = Convert.ToInt32(Console.ReadLine());
             string choice1 = myMapNod.Get(index);
-            Console.WriteLine("5th index values : "+ choice1);
-           
-            
+            Console.WriteLine("5th index values : " + choice1);*/
+            string phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] words = phrase.Split(' ');
+            int a = 1;
+            foreach (var word in words)
+            {
+                myMapNod.Add(a, word);
+                a++;
+            }
+            int index = 18;
+            string choice = myMapNod.Get(index);
+            Console.WriteLine("{0}th index values : is {1}",index,choice);
+            int index2 = 15;
+            string choice2 = myMapNod.Get(index2);
+            Console.WriteLine("{0}th index values : is {1}", index2, choice2);
+
+
         }
     }
 }
