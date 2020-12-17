968d318fbeb26a17cc05e76681c1f69fc0bb446b
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-18 01:14:12 +0530

    [Pratibha Mastud] Add: Find Frequency of word in a sentence

---
+++ HAshtableDemo/HAshtableDemo/MyMapNod.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HAshtableDemo
+{
+    public class MyMapNod<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNod(int size) 
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        protected int GetArrayPosition(K key) 
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> items = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedlist.AddLast(items);
+        }
+
+        public struct KeyValue<K, V>
+        { 
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+        }
+    }
+
---
+++ HAshtableDemo/HAshtableDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HAshtableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNod<int, string> myMapNod = new MyMapNod<int, string>(15);
+            myMapNod.Add(0,"To");
+            myMapNod.Add(1, "be");
+            myMapNod.Add(2, "or");
+            myMapNod.Add(3, "not");
+            myMapNod.Add(4, "To");
+            myMapNod.Add(5, "be");
+            int index = Convert.ToInt32(Console.ReadLine());
+            string choice1 = myMapNod.Get(index);
+            Console.WriteLine("5th index values : "+ choice1);
+           
+            
+        }
+    }
+}
